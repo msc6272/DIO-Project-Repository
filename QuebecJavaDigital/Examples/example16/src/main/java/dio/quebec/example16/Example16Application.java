b655826b270317f78646347efcd4195eae3ed32b
msc6272 <37087829+msc6272@users.noreply.github.com>
2022-11-11 17:16:32 +0000

    Initital example16

---
+++ QuebecJavaDigital/Examples/example16/src/main/java/dio/quebec/example16/Example16Application.java
@@ -0,0 +1,13 @@
+package dio.quebec.example16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example16Application.class, args);
+	}
+
+}
