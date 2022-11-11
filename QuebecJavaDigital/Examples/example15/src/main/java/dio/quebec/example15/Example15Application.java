aa8565afccd2d1e239aebbd42e24d9d055c0d57f
msc6272 <37087829+msc6272@users.noreply.github.com>
2022-11-11 02:03:33 +0000

    Inicio

---
+++ QuebecJavaDigital/Examples/example15/src/main/java/dio/quebec/example15/Example15Application.java
@@ -0,0 +1,13 @@
+package dio.quebec.example15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example15Application.class, args);
+	}
+
+}
---
+++ QuebecJavaDigital/Examples/example15/src/test/java/dio/quebec/example15/Example15ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.quebec.example15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
